ba34170ff137adbd84a214f2efd0acd423456c08
carlosvds <carloseduardovds@gmail.com>
2024-05-21 22:11:22 -0300

    Uncommenting code.

--- 01-LogicaDeProgramação/formatString.py
+++ 01-LogicaDeProgramação/formatString.py
@@ -1,44 +1,44 @@
-# nome = 'Carlos Santos'
-# altura = 1.65
-# peso = 72
-# imc = peso / altura ** 2
+nome = 'Carlos Santos'
+altura = 1.65
+peso = 72
+imc = peso / altura ** 2
 
-# print(nome)
-# print(altura)
-# print(peso)
-# print(f'Seu IMC é: {imc:.2f}')
+print(nome)
+print(altura)
+print(peso)
+print(f'Seu IMC é: {imc:.2f}')
 
-# a = 1
-# b = 1.18
-# c ='ABC'
+a = 1
+b = 1.18
+c ='ABC'
 
-# string = 'a={nome1} b={nome2:.3f} c={nome3}'
+string = 'a={nome1} b={nome2:.3f} c={nome3}'
 
-# formate = string.format(nome1=a, nome2=b, nome3=c)
+formate = string.format(nome1=a, nome2=b, nome3=c)
 
-# print(formate)
+print(formate)
 
-# nome = "Carlos Eduardo Vieira dos Santos"
-# print(len(nome))
+nome = "Carlos Eduardo Vieira dos Santos"
+print(len(nome))
 
-# numero_str = input('Vou dobrar o número que digitar: ')
+numero_str = input('Vou dobrar o número que digitar: ')
 
-# try:
-#    print('STR: ', numero_str)
-#    numero_float = float(numero_str)
-#    print('FLOAT: ', numero_float)
-#    print(f'O dobro de {numero_str} é: {numero_float * 2}')
-# except:
-#     print('Isso não é número...')   
+try:
+   print('STR: ', numero_str)
+   numero_float = float(numero_str)
+   print('FLOAT: ', numero_float)
+   print(f'O dobro de {numero_str} é: {numero_float * 2}')
+except:
+    print('Isso não é número...')   
 
-# velocidade = 70
-# local_carro = 90
+velocidade = 70
+local_carro = 90
 
-# RODAR_1 = 60
-# LOCAL_1 = 100
-# RADAR_RANGE = 1
+RODAR_1 = 60
+LOCAL_1 = 100
+RADAR_RANGE = 1
 
-# if velocidade <= RADAR_1:
-#     print(f'Carro passou no radar_1 a {velocidade }km por hora.')
-# else:
-#     print(f'Acima da velocidade {velocidade} por hora')    
+if velocidade <= RADAR_1:
+    print(f'Carro passou no radar_1 a {velocidade }km por hora.')
+else:
+    print(f'Acima da velocidade {velocidade} por hora')    

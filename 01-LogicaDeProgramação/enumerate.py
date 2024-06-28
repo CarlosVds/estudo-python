c8da300f7c85f655c6a6cc6581e57fe03c104880
carlosvds <carloseduardovds@gamil.com>
2024-06-28 14:28:55 -0300

    Using enumerate in a list

---
+++ 01-LogicaDeProgramação/enumerate.py
@@ -0,0 +1,10 @@
+# nome1= ['Carlos','Marcella','Enzo']
+
+# nome2 = tuple(nome1)
+
+# print(nome2)
+
+lista_nome = ['Carlos','Marcella','Enzo']
+
+for indice, nomes in enumerate(lista_nome):
+    print(indice, nomes)

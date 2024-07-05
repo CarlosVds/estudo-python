fd06d3a7b8c4c1998384c68e6cbeb453df157685
carlosvds <carloseduardovds@gamil.com>
2024-07-05 18:54:37 -0300

    Formatting with the round() function

---
+++ 01-LogicaDeProgramação/06_round-decimal.py
@@ -0,0 +1,7 @@
+num1 = 0.1
+num2 = 0.2
+num3 = num1 * num2
+
+print(f'Sem formatação {num3}.')
+print(f'Formatação normal com ".2f" {num3:.2f}.')
+print(f'Formatando usando a função "round()" {round(num3, 2)}.')

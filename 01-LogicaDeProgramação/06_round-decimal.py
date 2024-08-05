23344af35898c30edb0963bdf897f1033b4130a2
carlosvds <carloseduardovds@gamil.com>
2024-08-05 14:12:52 -0300

    Formatting with round and Decimal

--- 01-LogicaDeProgramação/06_round-decimal.py
+++ 01-LogicaDeProgramação/06_round-decimal.py
@@ -1,7 +1,9 @@
-num1 = 0.1
-num2 = 0.2
-num3 = num1 * num2
+import decimal
 
-print(f'Sem formatação {num3}.')
-print(f'Formatação normal com ".2f" {num3:.2f}.')
-print(f'Formatando usando a função "round()" {round(num3, 2)}.')
+num1 = decimal.Decimal(str(0.1))
+num2 = decimal.Decimal(str(0.2))
+num3 = num1 + num2
+
+print(f'Sem formatação {num3}')
+print(f'Formatação normal com ".2f" {num3:.2f}')
+print(f'Formatando usando a função "round()" {round(num3, 2)}')
